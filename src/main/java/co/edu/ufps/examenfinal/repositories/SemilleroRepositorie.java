4aca243402a4e703563bc7121c825dc06a3baec5
Monrrad <ramondavidmc@ufps.edu.co>
2024-06-24 12:25:58 -0500

    update Semillero

--- src/main/java/co/edu/ufps/examenfinal/repositories/SemilleroRepositorie.java
+++ src/main/java/co/edu/ufps/examenfinal/repositories/SemilleroRepositorie.java
@@ -1,4 +1,7 @@
 package co.edu.ufps.examenfinal.repositories;
 
-public interface SemilleroRepositorie {
+import co.edu.ufps.examenfinal.entities.Semillero;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SemilleroRepositorie extends JpaRepository<Semillero, Integer> {
 }
--- src/main/java/co/edu/ufps/examenfinal/services/SemilleroService.java
+++ src/main/java/co/edu/ufps/examenfinal/services/SemilleroService.java
@@ -3,7 +3,11 @@ package co.edu.ufps.examenfinal.services;
 import co.edu.ufps.examenfinal.entities.Semillero;
 import co.edu.ufps.examenfinal.repositories.SemilleroRepositorie;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 public class SemilleroService {
@@ -11,8 +15,19 @@ public class SemilleroService {
     @Autowired
     private SemilleroRepositorie semilleroRepositorie;
 
+    public Semillero getSemilleroById(Integer id) {
+        Optional<Semillero> semillero = semilleroRepositorie.findById(id);
+        if(semillero.isPresent()) {
+            return semillero.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
 
     public Semillero UpdateSemillero() {
+
 
     }
 }
