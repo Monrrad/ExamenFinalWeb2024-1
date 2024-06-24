c6c623be3b7c171041c8616d8ce9a9979872e11a
Monrrad <ramondavidmc@ufps.edu.co>
2024-06-24 00:07:53 -0500

    Inicio

---
+++ src/test/java/co/edu/ufps/examenfinal/ExamenFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ufps.examenfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
